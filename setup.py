304cad163c1ed9fb3e328b3a66337068092e9fec
Fan Fangda <wsffd@qq.com>
2017-03-27 19:04:44 -0500

    set download_url in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Fangda Fan',
   author_email = 'wsffd@qq.com',
   url = 'https://github.com/founderfan/DFlearn', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/founderfan/DFlearn/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['machine-learning', 'pandas', 'scikit-learn', 'cross-validation'], # arbitrary keywords
   classifiers = [],
 )
