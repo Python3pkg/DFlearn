a316fcade6db9280c1b6bd14c72c987f784c0b71
Fan Fangda <wsffd@qq.com>
2017-04-18 19:04:09 -0500

    update version to 0.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'dflearn',
     packages = ['dflearn'], # this must be the same as the name above
-    version = '0.1.7',
+    version = '0.1.8',
     install_requires=[
         'numpy>=1.11.0',
         'scipy>=0.18.0',
@@ -17,7 +17,7 @@ setup(
     author_email = 'recreating@outlook.com',
     url = 'https://github.com/founderfan/DFlearn',
     download_url = 'https://github.com/founderfan/DFlearn/archive/0.1.7.tar.gz',
-    keywords = 'machine-learning pandas scikit-learn cross-validation',
+    keywords = ['machine-learning', 'pandas', 'scikit-learn', 'cross-validation'],
     classifiers = ['Programming Language :: Python :: 3'],
     license='MIT'
 )
